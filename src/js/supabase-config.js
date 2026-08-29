df8e87eda701e1f071af242c27c0344333681bd8
dawn4080 <dawn01542@gmail.com>
2026-08-29 21:05:42 +0900

    Supabase 로그인 추가: src/js/supabase-config.js

---
+++ src/js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://vqdlexfgsmvbdmypykja.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_aR4Uf1GmYOTyMrL-Ea3YMg_dIlptleq';
+
+export { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY };
