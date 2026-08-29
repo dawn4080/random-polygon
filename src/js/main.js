058963b8695ed8d96be312d7e083afa36cba5c09
dawn4080 <dawn01542@gmail.com>
2026-08-29 21:05:54 +0900

    Supabase 로그인 반영: src/js/main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import { canvas } from './config.js';
 import { GameManager } from './game-manager.js';
 import { bindUi } from './ui.js';
+import { initAuth } from './auth.js';
 
 const gm = new GameManager();
 globalThis.gm = gm;
 bindUi(gm, canvas);
+initAuth();
 
 function loop() {
   gm.update();
